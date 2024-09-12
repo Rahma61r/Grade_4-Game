4e95a2a14cf2232610804d9e19598924a0d6f387
Rahma61r <rahma5002ramadan@gmail.com>
2024-09-12 22:36:12 +0300

    coins

---
+++ Assets/collectcoins.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class collectcoins : MonoBehaviour
+{
+    private int Coin = 0; // تصحيح اسم المتغير
+    public TextMeshProUGUI CoinText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Coin") // تصحيح التحقق من العلامة
+        {
+            Coin++;
+            Debug.Log(Coin);
+            CoinText.text = Coin.ToString(); // تحديث النص في واجهة المستخدم
+            Destroy(other.gameObject);
+        }
+    }
+}
