6f66476182e51fdb9b044102afbca52912b57681
Rahma61r <rahma5002ramadan@gmail.com>
2024-09-28 02:07:44 +0300

    task1

---
+++ Assets/Dora.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus; // أصلح الخطأ هنا حيث يجب أن تكون Fungus
+
+public class Dora : MonoBehaviour
+{
+    public Flowchart myFlowchart;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Corrected method name and parameter type
+    void OnTriggerEnter(Collider x)
+    {
+        if (x.gameObject.tag == "Dora")
+        {
+            myFlowchart.ExecuteBlock("Dora");
+        }
+    }
+}
